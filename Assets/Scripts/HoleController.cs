cdc440f4299f8074c8ebcd3a1c9ac1063884f096
Sean Lange <seanlange@SRLMacBook-558.local>
2017-11-08 18:47:22 -0800

    working holes

---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour {
+
+	Transform plyer;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log("FALL");
+		// get and disable the collider on the line
+		GameObject.Find("Line").GetComponent<EdgeCollider2D>().enabled = false;
+		// set the player transform
+		plyer = GameObject.Find("Player").GetComponent<Transform>();
+		if (plyer.position.y > transform.position.y) {
+			// wait before pushing the ball behind
+			Invoke("PlayerFallThrough", .1f);
+		} else {
+			// push the ball behind right away
+            PlayerFallThrough();
+        }
+	}
+
+	// Pushes the ball back behind the table so it appears to fall through
+	void PlayerFallThrough ()
+	{
+		
+		plyer.position = new Vector3(plyer.transform.position.x, plyer.transform.position.y, 5);
+	}
+}
